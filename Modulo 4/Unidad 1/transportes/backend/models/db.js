574105ee001441d6035357e0b06941a8417d06f2
martindebenedetti <debenedetti.martin@gmail.com>
2023-07-23 18:33:07 -0300

    se agrega tarea M5U4

---
+++ Modulo 4/Unidad 1/transportes/backend/models/db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var util = require('util');
+
+
+var pool = mysql.createPool({
+    connectionlimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,    
+    database: process.env.MYSQL_DB_NAME,
+    port: process.env.MYSQL_PORT
+
+   
+})
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
